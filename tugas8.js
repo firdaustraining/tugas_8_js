902f81259ad49a8b8411943a7268c94a97f9d954
Faza <firdausauliafaza@gmail.com>
2021-05-11 09:03:28 +0700

    niomic.com

---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function ini() {
+  var firdaus = {
+    umur: 21,
+    lahir: "rembang",
+    ukuran_sepatu: 44,
+  };
+  console.log("sebelum ditambah ", firdaus);
+  firdaus.universitas = "muhammadiyah";
+  console.log("sesudah ditambah ", firdaus);
+}
+
+ini();
